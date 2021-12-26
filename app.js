ab172d85682a7b86a6eadb47a6885bee70d465df
YasinVeliyev <veliyev.yasin@gmail.com>
2021-12-26 20:53:35 +0400

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json 	modified:   public/css/templatemo-video-catalog.css 	modified:   views/add.ejs 	modified:   views/edit.ejs 	modified:   views/index.ejs 	modified:   views/photo.ejs

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
+const fs = require("fs");
+
 const Photo = require("./models/photoModel");
 
 const app = express();
@@ -8,7 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use("/", express.static("public"));
+app.use(fileUpload());
+app.use(
+	methodOverride("_method", {
+		methods: ["POST", "GET"],
+	}),
+);
 
 app.get("/", async (req, res, next) => {
 	const photos = await Photo.find({});
@@ -22,13 +32,51 @@ app.get("/about", (req, res, next) => {
 });
 
 app.post("/photos", async (req, res, next) => {
+	let uploadedImage = req.files.image;
+	let image =
+		"/uploads/" +
+		uploadedImage.name.split(".")[0] +
+		Date.now() +
+		"." +
+		uploadedImage.name.split(".")[1];
+	let uploadPath = __dirname + "/public" + image;
 	let { title, description } = req.body;
-	await Photo.create({ title, description });
+	uploadedImage.mv(uploadPath, async () => {
+		await Photo.create({ title, description, image });
+	});
+	res.redirect("/");
+});
+app.get("/photo/:photoId", async (req, res, next) => {
+	let photo = await Photo.findById(req.params.photoId);
+	res.render("photo", { photo });
+});
+
+app.get("/photo/edit/:photoId", async (req, res, next) => {
+	let photo = await Photo.findById(req.params.photoId);
+	res.render("edit", { photo });
+});
+
+app.put("/photo/:photoId", async (req, res, next) => {
+	let { title, description } = req.body;
+	let photo = await Photo.findByIdAndUpdate(req.params.photoId, { title, description });
+	res.redirect(`/photo/${req.params.photoId}`);
+});
+
+app.delete("/photo/:photoId", async (req, res, next) => {
+	let photo = Photo.findByIdAndDelete(req.params.photoId, (err, data) => {
+		fs.unlink(path.join(__dirname, "/public", data.image), (err) => {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log("File deleted");
+			}
+		});
+	});
 	res.redirect("/");
 });
 
 mongoose.connect("mongodb://localhost:27017/pcap").then(() => {
 	app.listen(5000, () => {
-		console.log("Sunucu 3000 portunda başlatıldı ...");
+		console.log("Sunucu 5000 portunda başlatıldı ...");
 	});
 });
